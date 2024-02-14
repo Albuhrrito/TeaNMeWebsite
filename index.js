c64f9996e94cd025b31c8352e3133cedc41a8e76
Albert Youssef <albuhrrito@users.noreply.github.com>
2024-02-13 23:53:39 -0800

    adjusted text and buttons

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function openMenu(evt, menuName) {
     var i, x, tablinks;
     x = document.getElementsByClassName("menu");
+    y = document.getElementById("orderButton");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
@@ -13,3 +14,6 @@ function openMenu(evt, menuName) {
     evt.currentTarget.firstElementChild.className += " w3-dark-grey";
   }
   document.getElementById("myLink").click();
+  function tabFunction() {
+    window.location.href = 'https://www.toasttab.com/6263793109-2256-colorado-blvd-108/v3/?mode=fulfillment';
+  }
